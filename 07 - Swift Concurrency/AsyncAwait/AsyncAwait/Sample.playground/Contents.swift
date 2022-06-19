31ef597158b37bd33665a1ebf569eab94d348ceb
rcanbaba <rcanbabaoglu@gmail.com>
2022-06-19 17:47:01 +0300

    - async await completed.

--- 07 - Swift Concurrency/AsyncAwait/AsyncAwait/Sample.playground/Contents.swift
+++ 07 - Swift Concurrency/AsyncAwait/AsyncAwait/Sample.playground/Contents.swift
@@ -18,7 +18,7 @@ enum TodoError: Error {
 }
 
 // MARK: - Service
-
+/*
 struct TodoService {
     
     func fetch(completion: @escaping (Result<[Todo], Error>) -> Void) {
@@ -55,13 +55,32 @@ struct TodoService {
         }.resume()
     }
 }
+*/
 
- let service = TodoService()
-service.fetch { res in
-    switch res {
-    case .success(let data):
-        print(data)
-    case .failure(let error):
+struct TodoService {
+    
+    func fetch() async throws -> [Todo] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/todos")!
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw TodoError.invalidRequest
+        }
+        
+        let decodedData = try JSONDecoder().decode([Todo].self, from: data)
+        return decodedData
+    }
+}
+
+let service = TodoService()
+
+Task {
+    do {
+        let todos = try await service.fetch()
+        dump(todos)
+    } catch {
         print(error)
     }
 }
+
+
